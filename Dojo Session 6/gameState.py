ad988f6adfcac86712f6ff83199db5e49f1903f2
havenfricke <havenfricke@u.boisestate.edu>
2026-03-19 15:15:36 -0600

    added coments to game state

--- Dojo Session 6/gameState.py
+++ Dojo Session 6/gameState.py
@@ -1,6 +1,6 @@
-bossTarget = None
+bossTarget = None   # set boss target to null, set instantiated Boss() object to this later
 
-currentRound = 1
+currentRound = 1    # set current round initial value to 1
 
-currentParty = []
+currentParty = []   # instantiate an empty arrya to store data in later
 
